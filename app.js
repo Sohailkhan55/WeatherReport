cd727363f6e815ddf8c8bd7b540f2a1ee272c8be
Mohd Sohail Ali Khan <122856021+Sohailkhan55@users.noreply.github.com>
2023-10-23 23:48:37 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,44 +1,67 @@
- const express = require("express");
-
+const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
+const apiKey = "ddb8d6a4609fc3d546ce18b54dac8a60";
+
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:true}));
-app.get("/", function(req,res){
-        res.sendFile(__dirname + "/index.html");
-        
+// Set EJS as templating engine
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+    res.render("index", { weather: null, description: null, weatherUrl: null, error: null });
 });
 
-app.post("/" , function(req , res){
-        const query = req.body.cityName;
-        const apiKey = "ddb8d6a4609fc3d546ce18b54dac8a60";
-        const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query +"&appid=" + apiKey;
-        https.get(url , function(response){
-            console.log(response.statusCode);
-            response.on("data" , function(data){
-                const weatherData = JSON.parse(data);
-                //console.log(weatherData);
-                const temp = weatherData.main.temp;
-                const weatherDescription = weatherData.weather[0].description;
+app.post("/", function (req, res) {
+
+    const query = req.body.cityName;
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query +"&appid=" + apiKey + "&units=" + unit;
+    https.get(url, function (response) {
+
+        console.log(response.statusCode);
+
+
+        response.on("data", function (data) {
+
+            const weatherData = JSON.parse(data);
+
+            // console.log(weatherData);
+
+            if (response.statusCode === 404) {
+                res.render("index", { weather: null, description: null, weatherUrl: null, error: weatherData.message })
+            }
+            else if(response.statusCode != 200)
+            {
+                res.render("index", { weather: null, description: null, weatherUrl: null, error: "Error, please try again!" })
+            }
+            else {
+
+                const temperature = weatherData.main.temp;
+                const description = weatherData.weather[0].description;
                 const icon = weatherData.weather[0].icon;
-                const imageURL = "http://openweathermap.org/img/wn/"+icon +"@2x.png";
-                res.write("<p>The weather description is " + weatherDescription +".</p>");
-                res.write("<h1>The temperature in " + query +" is " + temp + " degree Fahrenheit.</h1>");
-                res.write("<img src=" + imageURL + ">");
-                res.send();
-                
-            });
+                const imageUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+
+                console.log(temperature);
+                console.log(description);
+
+                // res.write("<h1>The temperature in " + query + " is " + temperature + " degrees Celsius.</h1>");
+                // res.write("<h3>The weather is currently " + description + "</h3>");
+                // res.write(" <img src=" + imageUrl + "> ");
+                // res.send();
+
+                const weatherText = "The temperature in " + query + " is " + temperature + " degrees Celsius.";
+                const weatherDescription = "The weather is currently " + description;
+                res.render("index", { weather: weatherText, description: weatherDescription, weatherUrl: imageUrl, error: null });
+            }
+
         });
-});
+    });
+})
 
-
-
-
-
-
-
-
-app.listen(3000,function(){
-    console.log("Server is running on port 3000");
-});
+app.listen(3000, function () {
+    console.log("Server is running on port 3000.");
+}); 
